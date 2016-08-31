500609fe191069e33aefbe3556d5352dddfe77a0
ashmeet <ashmeet.nas96@gmail.com>
2016-08-31 14:42:05 +0530

    Added error handling functions to $http.get functions and use ng-if in views.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,12 +7,19 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
+            $scope.showMenu = false;
+            $scope.message = "Loading ...";
+
 
             $scope.dishes= [];
             menuFactory.getDishes()
             .then(
                 function(response) {
                     $scope.dishes = response.data;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status+" "+response.statusText;
                 }
             );
 
@@ -77,11 +84,17 @@ angular.module('confusionApp')
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
             $scope.dish = {};
+            $scope.showDish = false;
+            $scope.message = "Loading ...";
+
             menuFactory.getDish(parseInt($stateParams.id,10))
             .then(
                 function(response){
                     $scope.dish = response.data;
                     $scope.showDish=true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status+" "+response.statusText;
                 }
             );
             
@@ -112,11 +125,17 @@ angular.module('confusionApp')
             $scope.promotion = menuFactory.getPromotion(0);
             $scope.chef = corporateFactory.getLeader(3);
             $scope.dish = {};
+            $scope.showDish = false;
+            $scope.message = "Loading ...";
+
             menuFactory.getDish(0)
             .then(
                 function(response){
                     $scope.dish = response.data;
-                    $scope.showDish = true;
+                    $scope.showDish=true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status+" "+response.statusText;
                 }
             );
 
